2264bcc7065745b5a16a6ba90137bce69b642d41
nowcow <49467190+nowcow@users.noreply.github.com>
2019-04-11 23:53:54 +0900

    Create Sample3.py

---
+++ Sample3.py
@@ -0,0 +1,12 @@
+sale = int(input("売上入力"))
+num = int(input("人数入力"))
+
+if sale >= 100 and num >= 30 :
+  print("大盛況")
+elif sale >= 100:
+  print("好調")
+elif sale >= 5-:
+  print("普通")
+else:
+  print("低調")
+print("処理終了")
